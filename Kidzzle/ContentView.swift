9a8c0ea9726dcc1c50ae8a028e3e5ba4373fd8bd
aphinyaCT <aphinyact@gmail.com>
2025-04-04 22:20:40 +0700

    Initial Commit

---
+++ Kidzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kidzzle
+//
+//  Created by aynnipa on 4/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kidzzle/KidzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidzzleApp.swift
+//  Kidzzle
+//
+//  Created by aynnipa on 4/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct KidzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
